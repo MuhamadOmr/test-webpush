84b4b853020953000cab614a6368fe4cf5c267ce
MuhamadOmr <mohamadomr33@gmail.com>
2017-08-24 19:09:11 +0200

    test

---
+++ server.js
@@ -0,0 +1,23 @@
+var restify = require('restify');
+var fs = require('fs');
+
+function respond(req, res, next) {
+  res.send('hello ' + req.params.name);
+  next();
+}
+
+var server = restify.createServer();
+
+// using body parser plugin for dealing with json in the
+// req.body
+server.use(restify.plugins.bodyParser());
+
+server.get('/', restify.plugins.serveStatic({
+  directory: './static',
+  file: 'index.html',
+  appendRequestPath: false
+}));
+
+server.listen(80, function() {
+  console.log('%s listening at %s', server.name, server.url);
+});
